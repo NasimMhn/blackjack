01cc28dfffdb376041ab1d186c9e071ff1f947a1
nasimmhn <nasim.mahzoun@gmail.com>
2020-02-23 20:15:56 +0100

    win condition functions, result msg

--- black-jack.js
+++ black-jack.js
@@ -1,6 +1,6 @@
 
 
-// shuffle cards
+// Shuffle cards
 const shuffleCards = (array) => {
   let currentIndex = array.length
   let temporaryValue
@@ -36,14 +36,21 @@ const createImg = (cards, parent) => {
   parent.appendChild(img);
 }
 
-// Count sum of card array's values 
+// Set score to score element
 const countScore = (cards, element) => {
-  let totalSum = 0
-  cards.map((card) => {
-    totalSum = totalSum + card.value
-  })
-  element.innerText = totalSum
+  totalScore = sumCards(cards)
+  element.innerText = totalScore
 }
+
+// Helper function that sums card values
+const sumCards = (cards) => {
+  let total = 0
+  cards.map((card) => {
+    total = total + card.value
+  })
+  return total
+}
+
 
 // Moves card from one deck to another
 const moveCard = (fromDeck, toDeck) => {
@@ -57,48 +64,70 @@ const playSound = () => {
   audio.play()
 }
 
+// Displays result
+const showResult = (message) => {
+  document.getElementById("result").classList.add("show-result")
+  document.getElementById("result").innerText = message
+}
 
 let allCards = [
   {
     "value": 1,
-    "type": "hearts",
     "img": "assets/cards/AH.png"
   },
   {
     "value": 2,
-    "type": "hearts",
     "img": "assets/cards/2H.png"
   },
   {
     "value": 3,
-    "type": "hearts",
     "img": "assets/cards/3H.png"
   },
   {
     "value": 4,
-    "type": "hearts",
     "img": "assets/cards/4H.png"
   },
   {
     "value": 5,
-    "type": "hearts",
     "img": "assets/cards/5H.png"
   },
   {
     "value": 6,
-    "type": "hearts",
     "img": "assets/cards/6H.png"
-  }
+  },
+  {
+    "value": 7,
+    "img": "assets/cards/7H.png"
+  },
+  {
+    "value": 8,
+    "img": "assets/cards/8H.png"
+  },
+  {
+    "value": 9,
+    "img": "assets/cards/9H.png"
+  },
+  {
+    "value": 10,
+    "img": "assets/cards/10H.png"
+  },
+  {
+    "value": 10,
+    "img": "assets/cards/JH.png"
+  },
+  {
+    "value": 10,
+    "img": "assets/cards/QH.png"
+  },
+  {
+    "value": 10,
+    "img": "assets/cards/KH.png"
+  },
 ]
 
-
+// Arrays with cards turned up on the table
 let dealerUpcards = []
 let playerUpcards = []
-
-let shuffledCards = shuffleCards(allCards)
-let { dealerDeck, playerDeck } = splitDeck(shuffledCards)
-
-
 
 // Score
 let dealerScore = document.getElementById('dealer-score')
@@ -108,28 +137,70 @@ let playerScore = document.getElementById('player-score')
 let dealerSection = document.getElementById('dealer-cards')
 let playerSection = document.getElementById('player-cards')
 
+let shuffledCards = shuffleCards(allCards)
+let { dealerDeck, playerDeck } = splitDeck(shuffledCards)
 
+// Play
 const play = () => {
-
   moveCard(playerDeck, playerUpcards)
   createImg(playerUpcards, playerSection)
   countScore(playerUpcards, playerScore)
   playSound()
-
+  let result = winner(dealerUpcards, playerUpcards)
+  if (result) return
   setTimeout(() => {
-    moveCard(dealerDeck, dealerUpcards)
-    createImg(dealerUpcards, dealerSection)
-    countScore(dealerUpcards, dealerScore)
-    playSound()
+    if (sumCards(dealerUpcards) < 18 || sumCards(dealerUpcards) < sumCards(playerUpcards)) {
+      moveCard(dealerDeck, dealerUpcards)
+      createImg(dealerUpcards, dealerSection)
+      countScore(dealerUpcards, dealerScore)
+      playSound()
+      winner(dealerUpcards, playerUpcards)
+    }
+  }, 700);
+
+
+}
+
+// Hold
+const hold = () => {
+  setTimeout(() => {
+    if (sumCards(dealerUpcards) < 18 || sumCards(dealerUpcards) < sumCards(playerUpcards)) {
+      moveCard(dealerDeck, dealerUpcards)
+      createImg(dealerUpcards, dealerSection)
+      countScore(dealerUpcards, dealerScore)
+      playSound()
+      winner(dealerUpcards, playerUpcards)
+    }
   }, 700);
 }
 
+const winner = (dealerUpcards, playerUpcards) => {
+  if (blackJack(playerUpcards) || bust(dealerUpcards) || sumCards(playerUpcards) === 21) {
+    showResult("You win!! 🏆")
+    return true
+  }
+  if (blackJack(dealerUpcards) || bust(playerUpcards) || sumCards(dealerUpcards) === 21) {
+    showResult("You lose! 😢")
+    return true
+  }
+  else false
+}
 
-const hold = () => {
-  setTimeout(() => {
-    moveCard(dealerDeck, dealerUpcards)
-    createImg(dealerUpcards, dealerSection)
-    countScore(dealerUpcards, dealerScore)
-    playSound()
-  }, 700);
+// Check if BlackJack
+const blackJack = (cards) => {
+  if (cards.length === 2) {
+    if ((cards[0].value === 1 && cards[1].value === 10) || (cards[1].value === 1 && cards[0].value === 10)) {
+      return true
+    }
+  }
+  else false
+}
+
+// If score > 21
+const bust = (cards) => {
+  const score = sumCards(cards)
+  if (score > 21) {
+    return true
+  }
+  return false
 }
