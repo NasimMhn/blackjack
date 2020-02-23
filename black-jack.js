66cd822c4f583eee5d6c472f17a93de72d4e77be
nasimmhn <nasim.mahzoun@gmail.com>
2020-02-23 17:09:36 +0100

    added score, play function works

--- black-jack.js
+++ black-jack.js
@@ -27,40 +27,57 @@ const splitDeck = (cards) => {
   return { dealerDeck, playerDeck }
 }
 
+// Creates new card (img) in parent
+const createImg = (cards, parent) => {
+  let img = document.createElement('img')
+  img.src = cards[0].img
+  img.className = "card"
+  parent.appendChild(img);
+}
+
+// Count sum of card array's values 
+const countScore = (cards, element) => {
+  let totalSum = 0
+  cards.map((card) => {
+    totalSum = totalSum + card.value
+  })
+  element.innerText = totalSum
+}
+
 // Moves card from one deck to another
 const moveCard = (fromDeck, toDeck) => {
-  toDeck.push(fromDeck[0])
+  toDeck.unshift(fromDeck[0])
   fromDeck.shift()
 }
 
 let allCards = [
   {
-    "id": 1,
+    "value": 1,
     "type": "hearts",
     "img": "assets/cards/AH.png"
   },
   {
-    "id": 2,
+    "value": 2,
     "type": "hearts",
     "img": "assets/cards/2H.png"
   },
   {
-    "id": 3,
+    "value": 3,
     "type": "hearts",
     "img": "assets/cards/3H.png"
   },
   {
-    "id": 4,
+    "value": 4,
     "type": "hearts",
     "img": "assets/cards/4H.png"
   },
   {
-    "id": 5,
+    "value": 5,
     "type": "hearts",
     "img": "assets/cards/5H.png"
   },
   {
-    "id": 6,
+    "value": 6,
     "type": "hearts",
     "img": "assets/cards/6H.png"
   }
@@ -76,20 +93,26 @@ let { dealerDeck, playerDeck } = splitDeck(shuffledCards)
 
 
 
+// Score
+let dealerScore = document.getElementById('dealer-score')
+let playerScore = document.getElementById('player-score')
 
-
+// Sections
+let dealerSection = document.getElementById('dealer-cards')
+let playerSection = document.getElementById('player-cards')
 
 
 const play = () => {
-  moveCard(dealerDeck, dealerUpcards)
-  createImg(dealerUpcards[0].img)
 
+  moveCard(playerDeck, playerUpcards)
+  createImg(playerUpcards, playerSection)
+  countScore(playerUpcards, playerScore)
+
+  setTimeout(() => {
+    moveCard(dealerDeck, dealerUpcards)
+    createImg(dealerUpcards, dealerSection)
+    countScore(dealerUpcards, dealerScore)
+  }, 700);
 }
 
 
-const createImg = (imgSrc) => {
-  let img = document.createElement('img')
-  img.src = imgSrc
-  img.className = "card"
-  document.getElementById('dealer-side').appendChild(img);
-}
